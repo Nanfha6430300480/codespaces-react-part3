738dbd8980d6e53251b7037cf2e57718adc11043
Nanfha Sutthikul <87272655+Nanfha6430300480@users.noreply.github.com>
2024-10-03 08:41:24 +0000

    Initial version

---
+++ src/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return (<h1>Home of this application</h1>)
+}
---
+++ src/Post-item.jsx
@@ -0,0 +1,8 @@
+export default function PostItem(props){
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
